3fa8f544e019166eb692cbde432c93e772c08b69
Suraj Kumar Singh <114074384+goodmorninggg@users.noreply.github.com>
2024-02-28 16:34:53 +0530

    Create LT-7 reverse a number.java

---
+++ LT-7 reverse a number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+       int rev=0;
+       int last;
+       int quot;
+        while(x!
+        0){
+            last= x%10;
+            rev= s*10+r;
+
+            quot= x/10;
+
+        }
+        return s;
+    }
+}
